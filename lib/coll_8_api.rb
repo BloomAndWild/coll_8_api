b79a67c66d040c42741d51d416b215c1f8f42fe7
Rachel Bingham <eddie901@gmail.com>
2020-08-27 12:06:34 +0100

    Implement basic Operation

--- lib/coll_8_api.rb
+++ lib/coll_8_api.rb
@@ -3,10 +3,15 @@
 require 'json'
 require 'faraday'
 
+require 'coll_8_api/errors/abstract_method_error'
+require 'coll_8_api/errors/response_error'
+
 require 'coll_8_api/config'
 require 'coll_8_api/client'
 require 'coll_8_api/auth_token'
 require 'coll_8_api/version'
+
+require 'coll_8_api/operation'
 
 module Coll8Api
 end
